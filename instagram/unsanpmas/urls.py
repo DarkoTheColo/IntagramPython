6fe78cb1f01803b1f3267a33ec133080bea82802
Centro Ático <centroatico@202C11E30198973.local>
2017-05-20 13:18:18 -0500

    faltan3semanas

--- instagram/unsanpmas/urls.py
+++ instagram/unsanpmas/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^registrar/$', views.registrar, name = 'registro'),
+    url(r'^crear_usuario/$', views.crear_usuario, name = 'crear_usuario'),
     url(r'^inicio_sesion/$', views.inicio, name = 'inicio'),
     url(r'^pagina_inicio/$', views.pagina, name = 'pagina'),
     url(r'^cuenta/$', views.cuenta, name = 'cuenta'),
--- instagram/unsanpmas/views.py
+++ instagram/unsanpmas/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def registrar(request):
     return render ( request, 'first_try.html')
+def crear_usuario( request ):
+    email= request.POST[ 'fristname' ]
+    username= request.POST[ 'lastname' ]
+    password= request.POST[ 'password' ]
+    repeat_password= request.POST[ 'repeat_password' ]
+    print (email)
+    print (username)
+    print (password)
+
 def inicio(request):
     return render ( request, 'inicio_sesion2.html')
 def pagina(request):
