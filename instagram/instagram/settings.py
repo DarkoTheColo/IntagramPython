7e9ec626cce338f9b4aa34e7073c1356b2cacbc5
Centro Ático <centroatico@202C11E30198973.local>
2017-05-27 12:54:00 -0500

    faltan2semanas

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'registrar'
+LOGIN_REDIRECT_URL = 'pagina'
+LOGOUT_REDIRET_URL = 'index'
--- instagram/unsanpmas/views.py
+++ instagram/unsanpmas/views.py
@@ -1,16 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from unsanpmas.models import *
 
 # Create your views here.
 def registrar(request):
     return render ( request, 'first_try.html')
+
 def crear_usuario( request ):
-    email= request.POST[ 'fristname' ]
-    username= request.POST[ 'lastname' ]
-    password= request.POST[ 'password' ]
-    repeat_password= request.POST[ 'repeat_password' ]
-    print (email)
-    print (username)
-    print (password)
+    _email= request.POST[ 'email' ]
+    _username= request.POST[ 'name' ]
+    _password= request.POST[ 'password' ]
+    _repeat_password= request.POST[ 'repeat_password' ]
+
+    if _password==_repeat_password:
+        print (_email)
+        print (_username)
+        print (_password)
+        print (_repeat_password)
+        user=User.objects.create_user ( username=_username, email=_email,
+        password=_password)
+        myUser = MiUsuario ( usuario = user )
+        myUser.save()
+        print (user)
+        print (user.password)
+        return redirect( 'inicio' )
+    else:
+        return render( request,'first_try.html' )
+
+
 
 def inicio(request):
     return render ( request, 'inicio_sesion2.html')
